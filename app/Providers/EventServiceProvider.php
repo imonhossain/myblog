f7d51d76bccdd2dca87648e9af21078546131189
“[shohel <[cse_shohel@yahoo.com]>
2017-03-26 22:50:13 +0600

    First Commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace myblog\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'myblog\Events\Event' => [
+            'myblog\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
